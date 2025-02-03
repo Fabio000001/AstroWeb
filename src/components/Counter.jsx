26d0b3c0ccc3ceed68a6784ae53bd9ac3c96828f
fabioacosta <fabioacosta00001@gmail.com>
2025-02-03 21:58:58 +0000

    PokeApi called

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,11 @@
 const Counter = () => {
+    async function Consulta() {
+        // const response = await fetch("https://pokeapi.co/api/v2/generation/1");
+        // const data = await response.json();
+        // console.log(data);
+    }
+    Consulta();
+
     return (
         <h1>Hola</h1>
     );
